abfc8984c1d7e41dbd71fb2d1efc336afe1ff6fc
Mose Schmiedel <mose@schmiednet.de>
2025-04-16 12:23:52 +0200

    Add debug print

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <autocharge/Autocharge.hpp>
 
 // put function declarations here:
-void start_chg(Master* master, SlaveData *slave)
+void start_chg(Master *master, SlaveData *slave)
 {
   Serial.printf("Execute 'start_chg for slave %u'\n", slave->id);
   master->enjoinCharge(slave);
 }
-void end_chg(Master* master, SlaveData *slave)
+void end_chg(Master *master, SlaveData *slave)
 {
   Serial.printf("Execute 'end_chg for slave %u'\n", slave->id);
   master->cancelCharge(slave);
@@ -20,14 +20,18 @@ Master master = Master(chargingSlaves,
                        start_chg,
                        end_chg);
 
-
 void setup()
 {
   // put your setup code here, to run once:
-  delay(1000);
-  master.begin();
+  delay(2000);
   Serial.begin(115200);
+  Serial.println("+-------------------------+");
+  Serial.println("| Charging Station Master |");
+  Serial.println("+-------------------------+");
+  Serial.println();
+  master.begin();
   Serial.printf("NodeID '%u'\n", master.communication.getNodeId());
+  master.infraredLight.front.sendFrequency(100);
 }
 
 void loop()
